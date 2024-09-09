3418ae5b479d19e9ddd8500712a4e3f2f3401e0b
Mnpezz <93685835+Mnpezz@users.noreply.github.com>
2024-09-09 14:53:06 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 # exit on error
 set -o errexit
 
+# Create and activate a virtual environment
+python -m venv venv
+source venv/bin/activate
+
 # Install Python dependencies
 pip install -r requirements.txt
 
